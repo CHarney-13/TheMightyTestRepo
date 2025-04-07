042a6635deaf497a77b6f0ca23e88042246796c1
CHarney-13 <CHarney3383@student.leedsbeckett.ac.uk>
2025-04-08 00:52:08 +0100

    [CODE] Maven example added for testing only

--- HelloWorld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello World but in C!");
-	
-	return 0;
-}
--- HelloWorld.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "Hello World but in C++!";
-	return 0;
-}
